1e7f4ab9a2e400b9d985bbb83a8dba459b04e929
AndreyAf <andreyp@welldone-software.com>
2015-12-02 12:01:39 +0200

    delete thing

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,30 +1,12 @@
 'use strict';
 (function() {
 
-function MainController($scope, $http, socket) {
+function MainController(Auth) {
   var self = this;
-  this.awesomeThings = [];
 
-  $http.get('/api/things').then(function(response) {
-    self.awesomeThings = response.data;
-    socket.syncUpdates('thing', self.awesomeThings);
-  });
-
-  this.addThing = function() {
-    if (self.newThing === '') {
-      return;
-    }
-    $http.post('/api/things', { name: self.newThing });
-    self.newThing = '';
+  this.isLoggedIn = function(){
+    return Auth.isLoggedIn;
   };
-
-  this.deleteThing = function(thing) {
-    $http.delete('/api/things/' + thing._id);
-  };
-
-  $scope.$on('$destroy', function() {
-    socket.unsyncUpdates('thing');
-  });
 }
 
 angular.module('icssApp')
