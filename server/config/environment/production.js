9a1cbf744c68e1e7df3d01fb2d1ca6781c34bb49
AndreyAf <andreyp@welldone-software.com>
2015-12-02 21:40:38 +0200

    Merge branch 'master' of https://github.com/AndreyAf/YO-ICSS
    Conflicts:
    	package.json

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -3,22 +3,25 @@
 // Production specific configuration
 // =================================
 module.exports = {
-  // Server IP
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
+  //// Server IP
+  //ip:       process.env.OPENSHIFT_NODEJS_IP ||
+  //          process.env.IP ||
+  //          undefined,
 
-  // Server port
-  port:     process.env.OPENSHIFT_NODEJS_PORT ||
-            process.env.PORT ||
-            8080,
+  //// Server port
+  //port:     process.env.OPENSHIFT_NODEJS_PORT ||
+  //          process.env.PORT ||
+  //          8080,
 
   // MongoDB connection options
   mongo: {
-    uri:    process.env.MONGOLAB_URI ||
-            process.env.MONGOHQ_URL ||
-            process.env.OPENSHIFT_MONGODB_DB_URL +
-            process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/generatorangularfullstackmaster'
-  }
+    //uri:    process.env.MONGOLAB_URI ||
+    //        process.env.MONGOHQ_URL ||
+    //        process.env.OPENSHIFT_MONGODB_DB_URL +
+    //        process.env.OPENSHIFT_APP_NAME ||
+    //        'mongodb://localhost/generatorangularfullstackmaster'
+    uri: "mongodb://ninickname:oneXone1@ds061974.mongolab.com:61974/icssyo"
+  },
+
+  seedDB: true
 };
