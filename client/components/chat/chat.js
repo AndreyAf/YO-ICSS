d8eae612f3741dd7815f793024441642ed36ea30
rape master <yashaev@gmail.com>
2015-12-05 09:22:14 -0800

    io reconfigure

--- client/components/chat/chat.js
+++ client/components/chat/chat.js
@@ -6,7 +6,7 @@
 
     // TODO: rewrite server address
     //var myIoSocket = io.connect('https://icss-yo-v1.herokuapp.com:3000');
-    var myIoSocket = ciIoSvc.connect("http://chat-rape.herokuapp.com:80");
+    var myIoSocket = ciIoSvc.connect("https://chat-rape.herokuapp.com:80");
 
     var socket = socketFactory({ioSocket: myIoSocket});
 
