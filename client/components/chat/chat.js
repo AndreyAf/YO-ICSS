54bd0b9077fe08b24909bcc17e404a3adc7d51d1
rape master <yashaev@gmail.com>
2015-12-06 02:18:24 -0800

    ...

--- client/components/chat/chat.js
+++ client/components/chat/chat.js
@@ -5,7 +5,7 @@
   function ChatSvc(socketFactory, Auth, ciIoSvc, ciSessionSvc) {
 
     //var ioSocket = ciIoSvc.connect('ws://chat-rape.herokuapp.com:3000'),
-    var ioSocket = ciIoSvc.connect('ws://chat-rape.herokuapp.com:3000'),
+    var ioSocket = ciIoSvc.connect('wss://chat-rape.herokuapp.com/'),
       socket = socketFactory({ioSocket: ioSocket});
 
     return {
