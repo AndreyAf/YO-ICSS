7a5b20d2fd58c6df1ce36f0805be75f552b9be1f
AndreyAf <andreyp@welldone-software.com>
2015-12-08 00:16:32 +0200

    stable heroku ?!

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -4,14 +4,10 @@
 // =================================
 module.exports = {
   // Server IP
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
+  ip:       process.env.IP || undefined,
 
   // Server port
-  port:     process.env.OPENSHIFT_NODEJS_PORT ||
-            process.env.PORT ||
-            8080,
+  port:     process.env.PORT || 3000,
 
   // MongoDB connection options
   mongo: {
