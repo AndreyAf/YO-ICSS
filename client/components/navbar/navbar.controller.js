b2a1ed93c4e658cfe55a8c1461f33414d038ce7c
AndreyAf <andreyp@welldone-software.com>
2015-12-15 12:53:10 +0200

    add navbar by role support

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -4,16 +4,21 @@ angular.module('icssApp')
   .controller('NavbarCtrl', function ($scope, Auth) {
     $scope.menu = [{
       'title': 'Home',
-      'state': 'main'
+      'state': 'main',
+      'isVisible': true
     }, {
       'title': 'Chat',
-      'state': 'chat'
+      'state': 'chat',
+      'isVisible': Auth.isAdmin()
       //,
       //'target': '_blank'
+    }, {
+      'title': 'Admin',
+      'state': 'admin',
+      'isVisible': Auth.isAdmin()
     }];
 
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
   });
